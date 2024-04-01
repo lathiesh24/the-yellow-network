c5a94ccd63435e63a7c3f7e822bc65b720fdd26c
rakesh <rakeshmahendran99@gmail.com>
2024-04-01 13:43:40 +0530

    changes in homepage

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -4,23 +4,41 @@ import { RxAvatar } from 'react-icons/rx';
 import NavBar from './Navbar';
 import DefaultCard from './DefaultCard';
 import Prompt from './Prompt';
+import axios from 'axios';
 
 export default function HomePage() {
   const [userMessages, setUserMessages] = useState<string[]>([]);
-  const [systemResponses, setSystemResponses] = useState<string[]>([]);
+  const [systemResponses, setSystemResponses] = useState([]);
 
-  const handleSaveInput = (input: string) => {
-    setUserMessages([...userMessages, input]);
-    const systemResponse = `System response to "${input}"`;
-    setSystemResponses([...systemResponses, systemResponse]);
+  const handleSaveInput = async (input: string) => {
+    console.log("inputinhomepage",input)
+    let userquery = {"userquery":input}
+    console.log("userquery",userquery)
+
+    setUserMessages([...userMessages, input]); 
+
+    try{
+      console.log("input in url",input)
+      console.log("api is called")
+      const response = await axios.post("http://127.0.0.1:8000/api/prompt/ragsearch/",userquery)
+      let startupResults = response.data
+      console.log("startupResults",startupResults)
+      setSystemResponses(prevResponses => [...prevResponses, startupResults]);
+      console.log("responseinprompt",response)
+    } catch(error) {
+      console.log("erroringettingstartups",error)
+    }
+
+ 
   };
-
+  console.log("systemResponsess",systemResponses)
+  console.log("userMessages",userMessages)
   const renderMessages = () => {
     const messages = [];
     for (let i = 0; i < userMessages.length || i < systemResponses.length; i++) {
       if (userMessages[i]) {
         messages.push(
-          <div key={`user-${i}`} className="flex flex-col gap-y-2 my-6 py-2 px-4 shadow-blue-200 shadow-sm rounded-lg">
+          <div key={`user-${i}`} className="flex flex-col gap-y-2 my-6 py-2 px-4 shadow-sm rounded-lg">
             <div className='flex gap-x-1'>
               <div className="text-sky-400">
                 <RxAvatar size={23} />
@@ -35,15 +53,28 @@ export default function HomePage() {
       }
       if (systemResponses[i]) {
         messages.push(
-          <div key={`system-${i}`} className="flex flex-col gap-y-2 my-6 py-2 px-4 bg-gray-700 shadow-yellow-200 shadow-sm rounded-lg">
+          <div key={`system-${i}`} className="flex flex-col gap-y-2 my-6 py-2 px-4 bg-white shadow-sm rounded-lg">
             <div className='flex gap-x-1'>
               <div className="text-yellow-400">
                 <RxAvatar size={23} />
               </div>
-              <div className='font-semibold text-white'>System</div>
+              <div className='font-semibold text-black'>Game plan</div>
             </div>
-            <div className='text-white'>
-              {systemResponses[i]}
+            <div className='text-black' key={`system-${i}`}>
+              <div>
+                <div className='grid grid-cols-3 font-semibold text-base'>
+                      <div>Startup Name</div>
+                      <div>Product/Service</div>
+                      <div>Website</div>
+                </div>
+                {systemResponses[i] && systemResponses[i].results.map((result:any,index:number)=> (
+                  <div className='grid grid-cols-3 mt-4 rounded shadow-md p-2'>
+                        <div>{result?.startup_name}</div>
+                        <div>{result?.startup_technology}</div>
+                        <div>{result?.startup_url}</div>
+                    </div>
+                ))}
+              </div>
             </div>
           </div>
         );
