dfff317d3d44cc71d1e06ce9a8cb6caa70194da1
rakesh <rakeshmahendran99@gmail.com>
2024-04-08 11:16:30 +0530

    changes in company pane

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -9,17 +9,13 @@ import { StartupType } from "../interfaces";
 import LeftFrame from "./LeftFrame/LeftFrame";
 
 export default function HomePage() {
-  const [userMessages, setUserMessages] = useState<string[]>([]);
-  const [systemResponses, setSystemResponses] = useState([]);
   const [messages, setMessages] = useState([])
   const [defaultPrompt, setDefaultPrompt] = useState<string>("");
   const [open, setOpen] = useState<boolean>(true);
   const [selectedStartup, setSelectedStartup] = useState<StartupType>();
   const [openCompanyPane, setOpenCompanyPane] = useState<boolean>(true);
   const [inputPrompt, setInputPrompt] = useState(defaultPrompt);
-  const [loader, setLoader] = useState<boolean>(false);
   const [openRightFrame, setOpenRightFrame] = useState<boolean>(true);
-  const [messageId, setMessageId] = useState(0)
 
   const handleToggleLeftFrameNavbar = () => {
     setOpen(!open);
@@ -37,41 +33,29 @@ export default function HomePage() {
     }
   };
 
+  console.log("openRightFrame",openRightFrame)
+
   const handleSaveInput = async (input: string) => {
-    console.log("inputinhomepage", input);
+
     let userquery = { userquery: input };
-    console.log("userquery", userquery);
-    setUserMessages([...userMessages, input]);
+
 
     setMessages(prevMessages => [...prevMessages, { question : input , response : "Loading"}]);
     try {
-      console.log("input in url", input);
-      console.log("api is called");
-      setLoader(true)
       const response = await axios.post(
         "http://127.0.0.1:8000/api/prompt/ragsearch/",
         userquery
       );
-      let startupResults = response.data;
-      if (response) {
-        setLoader(false);
-      }
-      console.log("apiresponse", response);
-
       setMessages([...messages, { question : input , response : response.data}]);
 
-      // Increment message ID for the next message
-      setMessageId(prevId => prevId + 1);
-      setSystemResponses((prevResponses) => [...prevResponses, startupResults]);
     } catch (error) {
       console.log("erroringettingstartups", error);
     }
   };
 
-  console.log("first",loader)
   const handleClickItem = (item: StartupType) => {
     setSelectedStartup(item);
-    setOpenCompanyPane(true);
+    setOpenRightFrame(true)
   };
 
   console.log("messages",messages)
@@ -160,8 +144,8 @@ export default function HomePage() {
             <div>
               <CompanyProfilePane
                 companyData={selectedStartup}
-                setOpenState={setOpenCompanyPane}
-                openState={openCompanyPane}
+                setOpenState={setOpenRightFrame}
+                openState={openRightFrame}
               />
             </div>
           )}
