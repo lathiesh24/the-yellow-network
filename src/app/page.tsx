65806939c9ee6a0329b8a3e8aa4bd237880f8d1e
lathiesh24 <lathumahi24@gmail.com>
2024-04-25 15:52:07 +0530

    recent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,43 @@
-"use client"
-
+"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import HomePage from "./components/HomePage";
 import LoginPage from "./login/page";
 
 export default function Page() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+    localStorage.getItem("userInfo") ? true : false
+  );
+  const [isInitialCheckDone, setIsInitialCheckDone] = useState<boolean>(false);
   const router = useRouter();
 
   useEffect(() => {
-    let userInfo = localStorage.getItem("userInfo");
-    let parsedUserInfo = userInfo ? JSON.parse(userInfo) : null;
+    // Check if the user is already authenticated before performing authentication
+    if (!isLoggedIn) {
+      let userInfo = localStorage.getItem("userInfo");
+      let parsedUserInfo = userInfo ? JSON.parse(userInfo) : null;
 
-    if (parsedUserInfo) {
-      setIsLoggedIn(true);
-    } else {
-      router.push("/login");
+      if (!parsedUserInfo) {
+        router.push("/login");
+      }
     }
+
+    setIsInitialCheckDone(true); // Mark the initial check as done
   }, []);
 
   console.log("isLoggedIn", isLoggedIn);
 
   return (
     <main className="">
-      {isLoggedIn ? (
-        <div className="">
-          <HomePage />
-        </div>
-      ) : (
-        <LoginPage />
+      {isInitialCheckDone && (
+        // Render the UI only after the initial check is done
+        isLoggedIn ? (
+          <div className="">
+            <HomePage />
+          </div>
+        ) : (
+          <LoginPage />
+        )
       )}
     </main>
   );
