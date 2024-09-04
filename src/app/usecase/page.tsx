f24dd77f8d3532505c5b6565218c6efae778d294
lathiesh24 <lathumahi24@gmail.com>
2024-09-04 16:16:47 +0530

    changes

--- src/app/usecase/page.tsx
+++ src/app/usecase/page.tsx
@@ -56,36 +56,45 @@ const UsecaseDescription: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex flex-col bg-[#005585] px-4 justify-center items-center text-center gap-8 py-8 mt-16">
-        <div className="font-semibold text-white text-3xl">
-          {usecase || "No Use Case Title Available"}
-        </div>
-        <div
-          className="text-54 text-lg font-medium bg-white mx-auto px-4 py-2 cursor-pointer rounded-md shadow-md"
-          onClick={handleExploreEcosystem}
-        >
-          Explore Ecosystem
-        </div>
-      </div>
+     <div className="relative flex flex-col justify-center items-center text-center gap-8 py-8 mt-16 bg-[#005585] ">
+  {/* Background Image */}
+  <div className="absolute inset-0 z-0 opacity-15">
+    <img src="bg-usecase.png" alt="Background" className="object-cover w-full h-full" />
+  </div>
 
-      <div className="mx-4 py-4 leading-9 text-xl">
+  {/* Text Container */}
+  <div className="relative z-10 text-white font-semibold text-2xl px-4 py-8">
+    {usecase || "No Use Case Title Available"}
+  </div>
+
+  {/* Explore Ecosystem Button */}
+  <div
+    className="relative z-10 text-sm font-medium bg-white mx-auto px-4 py-2 cursor-pointer rounded-md shadow-md"
+    onClick={handleExploreEcosystem}
+  >
+    Explore Ecosystem
+  </div>
+</div>
+
+
+      <div className="mx-4 py-4 leading-9 text-lg">
         {usecaseDescription && (
           <div>
-            <div className="font-semibold text-xl mb-2">Description</div>
+            <div className="font-semibold text-lg mb-2">Description</div>
             <div className="text-base">{usecaseDescription}</div>
           </div>
         )}
 
         {enhancement && (
           <div className="mt-4">
-            <div className="font-semibold text-xl mb-2">Enhancement</div>
+            <div className="font-semibold text-lg mb-2">Enhancement</div>
             <div className="text-base">{enhancement}</div>
           </div>
         )}
 
         {measureOfImpact && (
           <div className="mt-4">
-            <div className="font-semibold text-xl mb-2">Measure of Impact</div>
+            <div className="font-semibold text-lg mb-2">Measure of Impact</div>
             <div className="text-base">{measureOfImpact}</div>
           </div>
         )}
